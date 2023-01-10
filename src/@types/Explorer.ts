08a999f574ca1fb0fe0533a23f2cee94dce3d629
ViniDevBR <vinigtr386@gmail.com>
2023-01-09 22:25:32 -0300

    :fire: New types for mock API

---
+++ src/@types/Explorer.ts
@@ -0,0 +1,5 @@
+export interface IExplorer {
+  id: string
+  name: string
+  type: 'hashtag' | 'music'
+}
---
+++ src/@types/Users.ts
@@ -0,0 +1,9 @@
+export interface IUsers {
+  id: string
+  verified: boolean
+  name: string
+  avatarURL: string
+  description: string
+  likes: string
+  followers: string
+}
