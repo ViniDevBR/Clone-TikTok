15e6cf2a3c2fefde12ca2cd54bcefa8b3bc8597e
ViniDevBR <vinigtr386@gmail.com>
2023-01-09 22:25:49 -0300

    :fire: Mock API

---
+++ src/Mock/Explorer.ts
@@ -0,0 +1,54 @@
+import { IExplorer } from '../@types/Explorer'
+
+export const explorer: IExplorer[] = [
+  {
+    id: '1',
+    name: 'dicaspara2023',
+    type: 'hashtag'
+  },
+  {
+    id: '2',
+    name: 'felizanonovo',
+    type: 'hashtag'
+  },
+  {
+    id: '3',
+    name: 'animaisnotiktok',
+    type: 'hashtag'
+  },
+  {
+    id: '4',
+    name: 'feliznatal',
+    type: 'hashtag'
+  },
+  {
+    id: '5',
+    name: 'booktokbrasil',
+    type: 'hashtag'
+  },
+  {
+    id: '6',
+    name: 'Então vamo deixar baixo - humannxx3',
+    type: 'music'
+  },
+  {
+    id: '7',
+    name: 'Na Rebolada - Os Quebradeiras',
+    type: 'music'
+  },
+  {
+    id: '8',
+    name: 'Beat Automotivo - tan tan tan viral WZ beat',
+    type: 'music'
+  },
+  {
+    id: '9',
+    name: 'Cachorrinhas - Luiza Sonza',
+    type: 'music'
+  },
+  {
+    id: '10',
+    name: 'Dengo - João Gomes',
+    type: 'music'
+  },
+]
---
+++ src/Mock/TopUsers.ts
@@ -0,0 +1,85 @@
+import { IUsers } from '../@types/Users'
+
+export const topUsers: IUsers[] = [
+  {
+    id: '1',
+    name: 'Neymar JR',
+    verified: true,
+    avatarURL: 'https://img.redbull.com/images/c_crop,x_1489,y_0,h_1998,w_1998/c_fill,w_310,h_310/q_auto:low,f_auto/redbullcom/2022/11/23/bmjrydcmhq9ruadewjcf/futebol-neymar-jrs-five-final-mundial-2022',
+    description: 'Soccer Ball',
+    likes: '500M',
+    followers: '22M'
+  },
+  {
+    id: '2',
+    name: 'Messi',
+    verified: true,
+    avatarURL: 'https://conteudo.imguol.com.br/c/esporte/0f/2022/10/25/lionel-messi-comemora-apos-marcar-para-o-psg-contra-o-maccabi-haifa-pela-champions-league-1666731838458_v2_450x450.jpg',
+    description: 'Soccer Ball',
+    likes: '400M',
+    followers: '20M'
+  },
+  {
+    id: '3',
+    name: 'CR7',
+    verified: true,
+    avatarURL: 'https://www.bandab.com.br/wp-content/uploads/2021/06/CR7-960x960.jpg.webp',
+    description: 'Soccer Ball',
+    likes: '700M',
+    followers: '30M'
+  },
+  {
+    id: '4',
+    name: 'Mbappe',
+    verified: true,
+    avatarURL: 'https://conteudo.imguol.com.br/c/galeria/4e/2022/12/18/mbappe-faz-dois-gols-e-leva-final-da-copa-para-a-prorrogacao-1671383448531_v2_450x450.jpg',
+    description: 'Soccer Ball',
+    likes: '300M',
+    followers: '15M'
+  },
+  {
+    id: '5',
+    name: 'Vini JR',
+    verified: true,
+    avatarURL: 'https://i.superesportes.com.br/fp8jYGi8xUeTDG1o2XyBjORxMts=/1200x1200/smart/imgsapp.mg.superesportes.com.br/app/noticia_126420360808/2022/12/08/3983084/vini-jr-pela-selecao-brasileira-na-copa-do-mundo_1_25450.jpg',
+    description: 'Soccer Ball',
+    likes: '500M',
+    followers: '22M'
+  },
+  {
+    id: '6',
+    name: 'Gabi Gol',
+    verified: true,
+    avatarURL: 'https://img.estadao.com.br/fotos/crop/1200x1200/resources/jpg/6/5/1607891739856.jpg',
+    description: 'Soccer Ball',
+    likes: '500M',
+    followers: '22M'
+  },
+  {
+    id: '7',
+    name: 'Danilo',
+    verified: true,
+    avatarURL: 'https://conteudo.imguol.com.br/c/esporte/ef/2020/09/12/danilo-durante-treino-do-palmeiras-na-academia-de-futebol-1599946721332_v2_450x450.jpg',
+    description: 'Soccer Ball',
+    likes: '500M',
+    followers: '22M'
+  },
+  {
+    id: '8',
+    name: 'William',
+    verified: true,
+    avatarURL: 'https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2022/06/GettyImages-1399484391-e1654172509651.jpg?w=1200&h=1200&crop=1',
+    description: 'Soccer Ball',
+    likes: '500M',
+    followers: '22M'
+  },
+  {
+    id: '10',
+    name: 'Carlinhos',
+    verified: true,
+    avatarURL: 'https://tntsports.com.br/__export/1618788829810/sites/esporteinterativo/img/2021/04/18/agif21031318204774.jpg_1905016634.jpg',
+    description: 'Soccer Ball',
+    likes: '500M',
+    followers: '22M'
+  },
+]
