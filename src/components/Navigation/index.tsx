edd0c08312d3ef69aa1533d3dcbb55cbb6f6ded9
ViniDevBR <vinigtr386@gmail.com>
2023-01-09 22:26:50 -0300

    :fire: New navigation component

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,129 @@
+//REACT
+import { useState } from 'react'
+//ICONS
+import {
+  Hash,
+  House,
+  MusicNotesSimple,
+  Users,
+  VideoCamera
+} from 'phosphor-react'
+//STYLED-COMPONENTS
+import {
+  NavContainer,
+  IconContainer,
+  TopUsersContainer,
+  Login,
+  ExplorerContainer,
+  AboutContainer
+} from './styles'
+//API MOCK
+import { topUsers } from '../../Mock/TopUsers'
+import { explorer } from '../../Mock/Explorer'
+//TYPES
+import { IUsers } from '../../@types/Users'
+import { IExplorer } from '../../@types/Explorer'
+//COMPONENTS
+import { Button } from '../Button'
+import { UserList } from '../UserList'
+
+
+export function Navigation() {
+  const [userList] = useState<IUsers[]>(topUsers)
+  const [explorerList] = useState<IExplorer[]>(explorer)
+
+  return(
+    <NavContainer>
+      <IconContainer>
+        <ul className='listOfItems'>
+          <li className='navItems'>
+            <House size={40} color="#FF3B5C" weight="duotone" />
+            <p className='screen'>Para Você</p>
+          </li>
+          <li className='navItems'>
+            <Users size={40} color="#FFFFFFE6" weight="light" />
+            <p className='screen'>Seguindo</p>
+          </li>
+          <li className='navItems'>
+            <VideoCamera size={40} color='#FFFFFFE6' weight='light' />
+            <p className='screen'>LIVE</p>
+          </li>
+        </ul>
+
+        <Login>
+          <p className='alert'>
+            Faça login para seguir criadores, curtir vídeos e ver comentários.
+          </p>
+          <Button variant='terciary' title='Login'/>
+        </Login>
+      </IconContainer>
+
+      <TopUsersContainer>
+        <ul className='userList'>
+          <p className='userSuggestion'>Contas Sugeridas</p>
+
+          {userList.map(user => (
+            <UserList
+              key={user.id}
+              user={user}
+            />
+          ))}
+        </ul>
+      </TopUsersContainer>
+
+      <ExplorerContainer>
+        <p className='explorer'>Descobrir</p>
+
+        <ul className='explorerList'>
+          {explorerList.map(explorer => (
+            <li className='explorerItem' key={explorer.id}>
+              {explorer.type === 'hashtag' && <Hash size={20} color="#FFF" weight="regular" />}
+              {explorer.type === 'music' && <MusicNotesSimple size={20} color="#FFF" weight="regular" />}
+
+              <span className='explorerName'>{explorer.name}</span>
+            </li>
+          ))}
+        </ul>
+      </ExplorerContainer>
+
+      <AboutContainer>
+        <div className='about'>
+          <a className='aboutLink' href="#">Sobre</a>
+          <a className='aboutLink' href="#">Sala de imprensa</a>
+          <a className='aboutLink' href="#">Contato</a>
+          <a className='aboutLink' href="#">Carreira</a>
+          <a className='aboutLink' href="#">ByteDance</a>
+        </div>
+
+        <div className='about'>
+          <a className='aboutLink' href="#">TikTok for Good</a>
+          <a className='aboutLink' href="#">Anunciar</a>
+          <a className='aboutLink' href="#">Developers</a>
+          <a className='aboutLink' href="#">Transparency</a>
+          <a className='aboutLink' href="#">Recompensas</a>
+          <a className='aboutLink' href="#">TikTok</a>
+          <a className='aboutLink' href="#">TikTok Browse</a>
+          <a className='aboutLink' href="#">TikTok Embeds</a>
+        </div>
+
+        <div className='about'>
+          <a className='aboutLink' href="#">Ajuda</a>
+          <a className='aboutLink' href="#">Segurança</a>
+          <a className='aboutLink' href="#">Termos</a>
+          <a className='aboutLink' href="#">Privacidade</a>
+          <a className='aboutLink' href="#">Creator Portal</a>
+          <a className='aboutLink' href="#">Diretrizes da Comunidade</a>
+          <a className='aboutLink' href="#">Direitos Autorais</a>
+        </div>
+
+        <div className='about'>
+          <a className='aboutLink' href="#">Mais</a>
+        </div>
+
+        <div className='about'>
+          <p className='aboutLink disabled'>© 2023 TikTok</p>
+        </div>
+      </AboutContainer>
+    </NavContainer>
+  )
+}
---
+++ src/components/Navigation/styles.ts
@@ -0,0 +1,202 @@
+import styled from 'styled-components'
+
+export const NavContainer = styled.nav`
+  position: fixed;
+  top: 70px;
+  border-right: 1px solid #FFFFFF1F;
+  width: 70px;
+  height: calc(100vh - 70px);
+  padding: 12px 8px;
+  overflow-y: scroll;
+
+  @media (min-width: 1024px) {
+    width: 340px;
+  }
+`
+
+export const IconContainer = styled.div`
+  border-bottom: 1px solid #FFFFFF1F;
+  padding-bottom: 8px;
+
+  .listOfItems {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .navItems {
+    padding: 8px;
+    border-radius: 8px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    width: 100%;
+
+    &:hover {
+      background-color: #2E2E2E;
+    }
+    .screen {
+      display: none;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .navItems {
+      .screen {
+        display: block;
+        font-size: 1.6rem;
+        font-weight: 600;
+        color: #FFFFFFE6;
+      }
+    }
+  }
+`
+
+export const TopUsersContainer = styled.div`
+  padding-block: 8px;
+
+  .userSuggestion,
+  .contentUser {
+    display: none;
+  }
+
+  .userList {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .userItem {
+    padding: 8px;
+    border-radius: 8px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    width: 100%;
+
+    &:hover {
+      background-color: #2E2E2E;
+    }
+  }
+  .userImg {
+    border-radius: 50%;
+    width: 35px;
+    height: 35px;
+  }
+
+  @media (min-width: 1024px) {
+    .userSuggestion {
+      width: 100%;
+      display: block;
+      color: #FFFFFFBF;
+      margin-block: 8px;
+      padding-left: 8px;
+      font-weight: 600;
+    }
+    .contentUser {
+      display: flex;
+      flex-direction: column;
+    }
+    .nameUser {
+      color: #FFFFFFE6;
+      font-weight: 600;
+      font-size: 1.6rem;
+      margin-right: 5px;
+    }
+    .descUser {
+      color: #FFFFFFBF;
+    }
+  }
+`
+
+export const Login = styled.div`
+  display: none;
+
+  @media (min-width: 1024px) {
+    display: block;
+    padding: 20px 8px;
+
+    .alert {
+      font-size: 1.6rem;
+      color: #FFFFFF80;
+      margin-bottom: 20px;
+    }
+  }
+`
+
+export const ExplorerContainer = styled.div`
+  display: none;
+
+  @media (min-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+    padding: 8px;
+    border-top: 1px solid #FFFFFF1F;
+
+    .explorer {
+      width: 100%;
+      display: block;
+      color: #FFFFFFBF;
+      margin-block: 8px;
+      font-weight: 600;
+    }
+    .explorerList {
+      display: flex;
+      flex-wrap: wrap;
+      margin-top: 16px;
+    }
+    .explorerItem {
+      display: flex;
+      align-items: center;
+      gap: 5px;
+      padding: 3px 10px;
+      margin: 0 8px 12px 0;
+      background-color: #FFFFFF14;
+      border-radius: 500px;
+      cursor: pointer;
+
+      &:hover {
+        background-color: #FFFFFF05;
+      }
+    }
+    .explorerName {
+      color: #FFFFFFBF;
+    }
+  }
+`
+
+export const AboutContainer = styled.div`
+  display: none;
+
+  @media (min-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+    padding: 8px;
+    border-top: 1px solid #FFFFFF1F;
+    gap: 10px;
+    align-items: flex-start;
+
+    .about {
+      display: flex;
+      flex-wrap: wrap;
+
+      .aboutLink {
+        color: #FFFFFF80;
+        margin: 5px 6px 0 0;
+        font-weight: 600;
+
+        &:hover {
+          text-decoration: underline;
+        }
+
+        &.disabled {
+          text-decoration: none
+        }
+      }
+
+    }
+  }
+`
