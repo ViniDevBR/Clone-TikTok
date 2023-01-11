c19c0b101a17d1fd587345b04dfbbe6f7f829aea
ViniDevBR <vinigtr386@gmail.com>
2023-01-10 22:35:56 -0300

    :fire: Finish Responsive

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,6 +5,9 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
 
+  @media (min-width: 1024px) {
+    align-items: center;
+  }
 `
 
 export const MainContainer = styled.main`
